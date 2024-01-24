cb6d075ce0596e937f4dc3c165df7b0b5bc4d831
8thGear-Tech <opeyemibioku@gmail.com>
2024-01-24 22:32:03 +0100

    updated the create post controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,17 +3,54 @@ import { User } from "../models/authModel.js";
 import { verifyJWT } from "../utils/jwtUtils.js";
 import { BadUserRequestError, NotFoundError } from "../error/error.js";
 
+// const postController = {
+//   createPostController: async (req, res) => {
+//     try {
+//       // Verify the token
+//       const decodedToken = verifyJWT(req.headers.authorization);
+//       console.log("Request Headers:", req.headers);
+
+//       // Assuming your token includes the user ID
+//       const userIdFromToken = decodedToken.id;
+//       console.log("Request Headers:", req.headers);
+
+//       const { title, content } = req.body;
+
+//       const user = await User.findById(userIdFromToken);
+
+//       if (!user) {
+//         throw new NotFoundError("User not found");
+//       }
+
+//       const newPost = await Post.create({
+//         title,
+//         content,
+//         author: user._id,
+//       });
+
+//       user.posts.push(newPost);
+//       await user.save();
+
+//       res.status(201).json({ post: newPost });
+//     } catch (error) {
+//       console.error(error);
+//       throw error;
+//     }
+//   },
+
 const postController = {
   createPostController: async (req, res) => {
     try {
-      // Verify the token
-      const decodedToken = verifyJWT(req.headers.authorization);
-      console.log("Request Headers:", req.headers);
+      // Verify the token directly within the endpoint
+      const authHeader = req.headers.authorization;
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new error();
+      }
 
-      // Assuming your token includes the user ID
-      const userIdFromToken = decodedToken.id;
-      console.log("Request Headers:", req.headers);
+      const token = authHeader.split(" ")[1];
+      const decodedToken = jwt.verify(token, config.jwt_secret_key);
 
+      const userIdFromToken = decodedToken._id;
       const { title, content } = req.body;
 
       const user = await User.findById(userIdFromToken);
@@ -31,13 +68,14 @@ const postController = {
       user.posts.push(newPost);
       await user.save();
 
-      res.status(201).json({ post: newPost });
+      const newToken = generateJWT(user);
+
+      res.status(201).json({ post: newPost, token: newToken });
     } catch (error) {
       console.error(error);
       throw error;
     }
   },
-
   getPostsController: async (req, res) => {
     try {
       // Verify the token
