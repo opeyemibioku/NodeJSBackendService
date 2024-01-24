a3b20d661c0a06b7a4e35ae9c88e520b34f1b885
8thGear-Tech <opeyemibioku@gmail.com>
2024-01-24 20:37:35 +0100

    updated the env variables

--- src/config/development.js
+++ src/config/development.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const development = {
-  MONGODB_CONNECTION_URL: "mongodb://127.0.0.1/nodejsbackendservice",
+  MONGODB_CONNECTION_URL: process.env.DEV_MONGODB_CONNECTION_URL,
   PORT: 5000,
   jwt_secret_key: process.env.DEV_JWT_SECRET,
 };
--- src/config/production.js
+++ src/config/production.js
@@ -1,8 +1,7 @@
-// const production = {
-//   MONGODB_CONNECTION_URL:
-//     "mongodb+srv://opeyemibioku:swiftsavingsbank*008@swiftsavingsbank.ct803tg.mongodb.net/swiftsavingsbank?retryWrites=true&w=majority",
-//   PORT: 5000,
-// jwt_secret_key: process.env.PRODUCTION_JWT_SECRET,
-// };
+const production = {
+  MONGODB_CONNECTION_URL: process.env.PRODUCTION_MONGODB_CONNECTION_URL,
+  PORT: 5000,
+  jwt_secret_key: process.env.PRODUCTION_JWT_SECRET,
+};
 
-// export default production;
+export default production;
